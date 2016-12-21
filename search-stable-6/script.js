d5b650716e09116712e36a8aa0baa5da36ef30e3
CookieDrawing <kinsei.an@gmail.com>
2016-12-21 11:10:18 +0100

    Add files via upload

--- search-stable-6/script.js
+++ search-stable-6/script.js
@@ -1,18 +1,12 @@
-// When the page is loaded...
-window.onload = function() {
+function modalImage() {	
 	//Get the image, the modal, and the modals image.
 	var modal = document.getElementById('the_modal');
-
-	var img = document.getElementById('focus_image');
 	var modalImg = document.getElementById('img01');
-	//When the 'normal' image is clicked...
-	img.onclick  = function() {
-		//...show the modal.
-		modal.style.display = "block";
-		modalImg.src = this.src;
-	}
-	//Close the modal when the 'X' is clicked.
-	var span = document.getElementById("close");
+	var focusImg = document.getElementById('focus_image');
+		
+	//...show the modal. 
+	modal.style.display = "block";
+	modalImg.src = focusImg.src;
 
 	modal.onclick = function() {
 		modal.style.display = "none";
